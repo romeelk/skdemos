1646dbff4ad8603421edcaf40f6cb103d2d574a6
romeelk <romeel_k@yahoo.co.uk>
2025-04-17 17:03:25 +0100

    Added more prompt examples

---
+++ src/passingargs/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var (deploymentName, endpoint, apiKey) = ReadSettings();
+        // Initialize the kernel
+        var kernel = Kernel.CreateBuilder()
+            .AddAzureOpenAIChatCompletion(
+                deploymentName: deploymentName,
+                endpoint: endpoint,
+                apiKey: apiKey)
+            .Build();
+
+        // Create a simple prompt
+        var prompt = """
+                    You are a helpful knowledge base about world cities.
+                    Keep the summary to 2 facts.
+                    Give some interesting facts about {{$city}}
+                    """;
+
+        // Run the prompt
+        var arguments = new KernelArguments { ["city"] = "London" };
+        var cityFunction = kernel.CreateFunctionFromPrompt(prompt);
+
+        var result = await kernel.InvokeAsync(cityFunction, arguments);
+    
+        Console.WriteLine(result);
+
+        await ProcessImagePrompt(kernel);
+    }
+
+    static async Task ProcessImagePrompt(Kernel kernel)
+    {
+        var prompt = "You are a image assitant. Describe the image in {{$imageData}}";
+        var arguments = new KernelArguments(new Dictionary<string, object?>
+        {
+            {"imageData", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAAXNSR0IArs4c6QAAACVJREFUKFNj/KTO/J+BCMA4iBUyQX1A0I10VAizCj1oMdyISyEAFoQbHwTcuS8AAAAASUVORK5CYII="}
+        });
+        var imagePromptFunction = kernel.CreateFunctionFromPrompt(prompt);
+        var response = await kernel.InvokeAsync(imagePromptFunction, arguments);
+
+        Console.WriteLine($"Response: {response}");
+    }
+
+    private static (string deploymentName, string endpoint, string apiKey) ReadSettings()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        return (
+            config["AzureOpenAI:DeploymentName"] ?? throw new Exception("DeploymentName not found"),
+            config["AzureOpenAI:Endpoint"] ?? throw new Exception("Endpoint not found"),
+            config["AzureOpenAI:ApiKey"] ?? throw new Exception("ApiKey not found")
+        );
+    }
+}
--- src/promptexamples/Program.cs
+++ src/promptexamples/Program.cs
@@ -34,12 +34,16 @@ builder.Plugins.AddFromType<ConversationSummaryPlugin>();
 
 var kernel = builder.Build();
 
-try {
-    await PromptWithHistory(kernel);
-    await PromptTravelPersona(kernel);
-    await PromptSystemMessage(kernel);
+try
+{
+    // await PromptWithHistory(kernel);
+    // await PromptTravelPersona(kernel);
+    // await PromptSystemMessage(kernel);
+    //await ZeroShotPrompt(kernel);
+    //await FewShotPrompt(kernel);
+    await ChainOfThought(kernel);
 }
-catch(Exception exception)
+catch (Exception exception)
 {
     System.Console.WriteLine($"Oops an error occured {exception.ToString()}");
 }
@@ -109,5 +113,64 @@ static async Task PromptSystemMessage(Kernel kernel)
 
     Console.WriteLine($"Prompt {prompt}");
     var result = await kernel.InvokePromptAsync(prompt);
+    Console.WriteLine($"Prompot response: {result}");
+}
+
+/*
+*  Unguided with no context given
+*/
+static async Task ZeroShotPrompt(Kernel kernel)
+{
+    string request = "I want to get my expense done today";
+    string prompt = $"""
+    Instructions: What is the intent of this request?
+    If you don't know the intent, don't guess; instead respond with "Unknown".
+    Choices: CreateExpense, MakePayment, SendExpense,  Unknown.
+    User Input: {request}
+    Intent: 
+    """;
+    Console.WriteLine($"Prompt {prompt}");
+    var result = await kernel.InvokePromptAsync(prompt);
+    Console.WriteLine($"Prompot response: {result}");
+}
+
+/*
+*  Guided with specific examples with expected inputs and outputs
+*/
+static async Task FewShotPrompt(Kernel kernel)
+{
+    string request = "When i get back from the conference i need to sort out my hotel and flight expense";
+   string prompt = $"""
+Instructions: What is the intent of this request?
+If you don't know the intent, don't guess; instead respond with "Unknown".
+Choices: CreateExpense, MakePayment, SendExpense,, Unknown.
+
+User Input: Can you please approve my patment for my expense claim
+Intent: MakePayment
+
+User Input: I need to claim omy Hotel payment
+Intent: CreateExpense
+
+User Input: {request}
+Intent:
+""";
+    Console.WriteLine($"Prompt {prompt}");
+    var result = await kernel.InvokePromptAsync(prompt);
+    Console.WriteLine($"Prompot response: {result}");
+}
+
+/*
+* LLMs do not really reason like Humans and can be thrown by simple logical reasoning tasks.
+ Chain of thought is a approach to guide the LLM to break down the prompt ask into a series of steps.
+*/
+static async Task ChainOfThought(Kernel kernel)
+{
+   string prompt = $"What is the value of 3 + 4 + 19 - 12?";
+    Console.WriteLine($"Prompt {prompt}");
+    var kernelArgs = new KernelArguments{
+        {"temperature",   1.8}
+    };
+
+    var result = await kernel.InvokePromptAsync(prompt,kernelArgs);
     Console.WriteLine($"Prompot response: {result}");
 }
